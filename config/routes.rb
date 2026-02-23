fc3f05189325406a8a1b9667524039cae959a99d
Allan <77512326+Allanqan@users.noreply.github.com>
2026-02-23 16:16:26 +0100

    Fix broken devise_scope route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,7 @@ Rails.application.routes.draw do
   resources :companies
   resources :company_types
   resources :user_categories
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users
-  devise_scope :user do
-    get "/users/sign_up" => "users_controller"
-  end
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
